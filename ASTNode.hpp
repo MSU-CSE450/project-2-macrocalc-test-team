98ee9b9e01b2031e6fe0df0b7c7c9f8888f0a4ae
Luke Bridges <lukebridges123@gmail.com>
2024-10-02 10:14:11 -0400

    Rough sketch of the Run function, including declarations of helper functions and comments explaining what they should do

--- ASTNode.hpp
+++ ASTNode.hpp
@@ -17,13 +17,15 @@ public:
     SCOPE,
     PRINT,
     ASSIGN,
+    IDENTIFIER,
     CONDITIONAL,
     OPERATION,
     NUMBER,
     STRING
   };
   const Type type{EMPTY};
-  std::string literal{""};
+  std::string literal{""}; //can also serve as variable name when the node is of type IDENTIFIER, or an operation name
+                          //if of type OPERATION. Might also change things so we have another enum of operator types.
   double value{0};
   // CONSTRUCTORS, ETC HERE.
   // CAN SPECIFY NODE TYPE AND ANY NEEDED VALUES HERE OR USING OTHER FUNCTIONS.
@@ -36,6 +38,67 @@ public:
   }
   
   // CODE TO EXECUTE THIS NODE (AND ITS CHILDREN, AS NEEDED).
-  std::optional<double> Run(SymbolTable & symbols) { ; }
-
+  std::optional<double> Run(SymbolTable & symbols) { 
+    switch (type){
+      case EMPTY:
+        return;
+      case SCOPE:
+        RunScope(symbols);
+        return;
+      case PRINT:
+        RunPrint(symbols);
+        return;
+      case ASSIGN:
+        RunAssign(symbols);
+        return;
+      case IDENTIFIER:
+        return RunIdentifier(symbols);
+      case CONDITIONAL:
+        RunConditional(symbols);
+        return;
+      case OPERATION:
+        return RunOperation(symbols);
+      case STRING:
+        //not really sure if we need to run these?
+        //the only context in which we need to worry about string nodes is when handling prints
+        //and we can just have RunPrint access the "literal" of any string node
+        return;
+      default:
+        return;
+    }; 
+  }
+  //note: I would have made some of these SymbolTable references constant, but they'll be making recursive calls to Run,
+  //some of which will need to make changes to the symbol table, so none of them are constant except the one in RunIdentifier
+  void RunScope(SymbolTable & symbols){
+    //push a new scope
+    //run each child node in order
+    //pop scope
+  }
+  void RunPrint(SymbolTable & symbols){
+    //iterate over children
+    //if child is an expression or number, run it and print the value it returns
+    //if it's a string literal, print it
+    //need to do something about identifiers in curly braces
+  }
+  void RunAssign(SymbolTable & symbols){
+    //get variable name from first child
+    //possibly get a value from last child (which may be an expression to evaluate or a variable node)
+    //if it's a declaration, call AddVar
+    //if it's a declaration with initialization, call AddVar, then SetVar
+    //if it's just an assignment, only call SetVar
+  }
+  double RunIdentifier(const SymbolTable & symbols){
+    //just need to call GetValue on the node's literal (which should be the name of the variable), then return the result
+    return 0;
+  }
+  void RunConditional(SymbolTable & symbols){
+    //conditional statement is of the form "if (expression1) statment1 else statement2"
+    //so a conditional node should have 2 or 3 children: an expression, a statement, and possibly another statement
+    //run the first one; if it gives a nonzero value, run the second; otherwise, run the third, if it exists
+  }
+  double RunOperation(SymbolTable & symbols){
+    //node will have an operator (e.g. +, *, etc.) specified somewhere (maybe in the "literal"?) and one or two children
+    //run the child or children, apply the operator to the returned value(s), then return the result
+    return 0;
+  }
 };
