ea41ef0c24cc7fc40d6416cac578caaa8a0f3c9d
132ikl <132@ikl.sh>
2024-10-02 10:17:55 -0400

    Run clang-format and remove template comments

--- ASTNode.hpp
+++ ASTNode.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cmath>
+#include <optional>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -9,8 +10,8 @@
 class ASTNode {
 
 private:
-  // PLACE AST NODE INFO HERE.
   std::vector<ASTNode> children{};
+
 public:
   enum Type {
     EMPTY = 0,
@@ -25,17 +26,12 @@ public:
   const Type type{EMPTY};
   std::string literal{""};
   double value{0};
-  // CONSTRUCTORS, ETC HERE.
-  // CAN SPECIFY NODE TYPE AND ANY NEEDED VALUES HERE OR USING OTHER FUNCTIONS.
+
   ASTNode(Type type = EMPTY) : type(type) {};
   ASTNode(Type type, std::string literal) : type(type), literal(literal) {};
   ASTNode(Type type, double value) : type(type), value(value) {};
-  // CODE TO ADD CHILDREN AND SETUP AST NODE HERE.
-  void AddChild(ASTNode node) { 
-    children.push_back(node); 
-  }
-  
-  // CODE TO EXECUTE THIS NODE (AND ITS CHILDREN, AS NEEDED).
-  std::optional<double> Run(SymbolTable & symbols) { ; }
 
+  void AddChild(ASTNode node) { children.push_back(node); }
+
+  std::optional<double> Run(SymbolTable &symbols) { ; }
 };
--- Project2.cpp
+++ Project2.cpp
@@ -1,15 +1,13 @@
-#include <assert.h>
+#include <cassert>
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <unordered_map>
+
 #include <vector>
 
-// Below are some suggestions on how you might want to divide up your project.
-// You may delete this and divide it up however you like.
 #include "ASTNode.hpp"
-#include "lexer.hpp"
 #include "SymbolTable.hpp"
+#include "lexer.hpp"
 
 class MacroCalc {
 private:
@@ -19,37 +17,30 @@ private:
   size_t token_id{0};
   ASTNode root{ASTNode::SCOPE};
 
-
-
 public:
   MacroCalc() {};
-  void Parse(std::ifstream & input){
+  void Parse(std::ifstream &input) {
     tokens = lexer.Tokenize(input);
-    //parse everything into an AST
+    // parse everything into an AST
   }
-  void Execute(){
-    root.Run(table);
-  }
+  void Execute() { root.Run(table); }
 };
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char *argv[]) {
   if (argc != 2) {
     std::cout << "Format: " << argv[0] << " [filename]" << std::endl;
     exit(1);
   }
 
   std::string filename = argv[1];
-  
-  std::ifstream in_file(filename);              // Load the input file
+
+  std::ifstream in_file(filename); // Load the input file
   if (in_file.fail()) {
-    std::cout << "ERROR: Unable to open file '" << filename << "'." << std::endl;
+    std::cout << "ERROR: Unable to open file '" << filename << "'."
+              << std::endl;
     exit(1);
   }
 
-  // TO DO:  
-  // PARSE input file to create Abstract Syntax Tree (AST).
-  // EXECUTE the AST to run your program.
   MacroCalc calc = MacroCalc();
   calc.Parse(in_file);
   calc.Execute();
