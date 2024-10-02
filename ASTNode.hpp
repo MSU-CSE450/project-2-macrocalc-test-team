3f41c36769d6d27eb2119eae686a0b3b96a8a63a
Luke Bridges <lukebridges123@gmail.com>
2024-10-02 08:57:23 -0400

    Wrote a little bit of AST node code

--- ASTNode.hpp
+++ ASTNode.hpp
@@ -8,17 +8,34 @@
 #include "SymbolTable.hpp"
 
 class ASTNode {
+enum Type {
+  EMPTY = 0,
+  SCOPE,
+  PRINT,
+  ASSIGN,
+  CONDITIONAL,
+  OPERATION,
+  NUMBER,
+  STRING
+};
 private:
   // PLACE AST NODE INFO HERE.
-  
+  Type type{EMPTY};
+  std::string literal{""};
+  double value{0};
+  std::vector<ASTNode> children{};
 public:
   // CONSTRUCTORS, ETC HERE.
   // CAN SPECIFY NODE TYPE AND ANY NEEDED VALUES HERE OR USING OTHER FUNCTIONS.
-
+  ASTNode(Type type = EMPTY) : type(type) {};
+  ASTNode(Type type, std::string literal) : type(type), literal(literal) {};
+  ASTNode(Type type, double value) : type(type), value(value) {};
   // CODE TO ADD CHILDREN AND SETUP AST NODE HERE.
-  void AddChild(ASTNode node) { ; }
+  void AddChild(ASTNode node) { 
+    children.push_back(node); 
+  }
   
   // CODE TO EXECUTE THIS NODE (AND ITS CHILDREN, AS NEEDED).
   double Run(SymbolTable & symbols) { ; }
-
+  
 };
